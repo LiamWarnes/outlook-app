ecc98d30dd8b2542bd04c88aec0346fd0dc8186d
Liam Warnes <Liam.warnes@oneplacesolutions.com>
2024-07-02 15:04:58 +1000

    updated manifets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Signal, WritableSignal, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -12,7 +12,12 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'outlook-app';
 
+  protected _isApiSupported: WritableSignal<boolean> = signal(false);
+
+  protected isApiSupported: Signal<boolean> = this._isApiSupported.asReadonly();
+
   public async onLoginClick(): Promise<void> {
+    this._isApiSupported.set(Office.context.requirements.isSetSupported('IdentityAPI', '1.3'));
     const token: string = await Office.auth.getAccessToken({
       allowConsentPrompt: true,
       allowSignInPrompt: true
