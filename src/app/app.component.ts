50303779933add89fbdf1a55e8041cddd04636a5
Liam Warnes <Liam.warnes@oneplacesolutions.com>
2024-11-18 14:44:44 +1100

    naa bridge error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { createNestablePublicClientApplication, IPublicClientApplication } from '@azure/msal-browser';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,38 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'outlook-app';
+
+  private pca: IPublicClientApplication | undefined;
+
+  public async login(): Promise<void> {
+    if (Office.context.requirements.isSetSupported('NestedAppAuth', '1.1')) {
+      console.log('NestedAppAuth is supported');
+
+      if (!this.pca) {
+        this.pca = await createNestablePublicClientApplication({
+          cache: {
+            cacheLocation: 'localStorage',
+          },
+          auth: {
+            clientId: '5375418b-f2d2-460d-97bb-07d0fb552357',
+            redirectUri: `${window.location.origin}/auth.html`,
+            postLogoutRedirectUri: `${window.location.origin}/index.html`,
+          }
+        });
+      }
+
+      try {
+        const authResult = await this.pca.acquireTokenSilent({
+          scopes: ['openid', 'profile']
+        });
+
+        console.log('Successfully acquired token via NAA');
+        console.log(authResult);
+      } catch (error) {
+        console.log('Failed to acquire token via NAA');
+        console.error(error);
+      }
+      
+    }  
+  }
 }
